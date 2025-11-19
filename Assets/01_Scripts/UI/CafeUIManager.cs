3b3eea1c3c443b9550649a0494c71b439e2ab5d4
StylishBoi <max@rdh.ch>
2025-11-19 18:02:06 +0100

    - Organized the folders and removed useless scripts

--- Assets/01_Scripts/UI/CafeUIManager.cs
+++ Assets/01_Scripts/UI/CafeUIManager.cs
@@ -8,43 +8,44 @@ using UnityEngine.UI;
 
 public class CafeUIManager : MonoBehaviour
 {
-    [Header("UI Images")]
-    [SerializeField] private Image resultFlash;
+    [Header("UI Images")] [SerializeField] private Image resultFlash;
     [SerializeField] private Image bottomFlash;
     [SerializeField] private Image fullScreenFade;
     [SerializeField] private Image endHourScreen;
-    
-    [Header("Fade Color")]
-    [SerializeField] private Color badResultColor;
+
+    [Header("Fade Color")] [SerializeField]
+    private Color badResultColor;
+
     [SerializeField] private Color mediocreResultColor;
     [SerializeField] private Color goodResultColor;
     [SerializeField] private Color eventFlashColor;
-    
-    [Header("Other UI Elements")]
-    [SerializeField] private GameObject rushHourScreen;
+
+    [Header("Other UI Elements")] [SerializeField]
+    private GameObject rushHourScreen;
+
     [SerializeField] private GameObject rushOverScreen;
     [SerializeField] private GameObject inventoryScreen;
     [SerializeField] private TextMeshProUGUI streakText;
-    
+
     private GameObject UI;
     private UIFadeEffects _uiFadeEffects;
-    
-    
+
+
     public static CafeUIManager Instance;
-    
+
     void Awake()
     {
         _uiFadeEffects = GetComponent<UIFadeEffects>();
-        
+
         UI = transform.GetChild(0).gameObject;
-            
+
         if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
         else
-        { 
+        {
             Destroy(gameObject);
         }
     }
@@ -58,12 +59,12 @@ public class CafeUIManager : MonoBehaviour
     {
         UI.SetActive(false);
     }
-    
+
     public void HideInventory()
     {
         inventoryScreen.SetActive(false);
     }
-    
+
     public void ShowUI()
     {
         UI.SetActive(true);
@@ -78,40 +79,42 @@ public class CafeUIManager : MonoBehaviour
     {
         StartCoroutine(_uiFadeEffects.ImageFlashEvent(resultFlash, eventFlashColor, rushHourScreen));
     }
+
     public void RushOver()
     {
         StartCoroutine(_uiFadeEffects.ImageFlashEvent(resultFlash, eventFlashColor, rushOverScreen));
     }
-    
+
     public void StartOfDayFade()
     {
         StartCoroutine(_uiFadeEffects.DoFadeIn(fullScreenFade, Color.white));
         TimeManager.InTransition = false;
     }
-    
+
     public void EndOfDayFade()
     {
         TimeManager.InTransition = true;
         HideInventory();
         StartCoroutine(_uiFadeEffects.DoFadeOut(fullScreenFade, Color.black));
     }
-    
+
     public void DayOver()
     {
         TimeManager.InTransition = true;
         StartCoroutine(_uiFadeEffects.DoFadeOut(endHourScreen, Color.black));
         StartCoroutine(_uiFadeEffects.ImageFlashEvent(resultFlash, eventFlashColor));
     }
+
     public void Mediocre()
     {
         StartCoroutine(_uiFadeEffects.ImageFlashEvent(resultFlash, mediocreResultColor));
     }
-    
+
     public void NewItem()
     {
         StartCoroutine(_uiFadeEffects.ImageFlashEvent(bottomFlash, Color.white));
     }
-    
+
     public void Failure()
     {
         StartCoroutine(_uiFadeEffects.ImageFlashEvent(resultFlash, badResultColor));
--- Assets/01_Scripts/UI/UIFadeEffects.cs
+++ Assets/01_Scripts/UI/UIFadeEffects.cs
@@ -4,28 +4,30 @@ using UnityEngine.UI;
 
 public class UIFadeEffects : MonoBehaviour
 {
-    [Header("Effects Durations")]
-    [SerializeField] private float flashDuration=1f;
-    [SerializeField] private float fadeOutDuration=1f;
-    [SerializeField] private float fadeInDuration=1f;
+    [Header("Effects Durations")] [SerializeField]
+    private float flashDuration = 1f;
+
+    [SerializeField] private float fadeOutDuration = 1f;
+    [SerializeField] private float fadeInDuration = 1f;
+
     private Image TemporaryVariableMaker(Image imageSample, Color colorSample)
     {
-        Image tempImage=Instantiate(imageSample, imageSample.transform.parent);
-        
+        Image tempImage = Instantiate(imageSample, imageSample.transform.parent);
+
         tempImage.gameObject.SetActive(true);
         return tempImage;
     }
-    
+
     public IEnumerator ImageFlashEvent(Image fadeImage, Color fadeColor)
     {
         var tempImage = TemporaryVariableMaker(fadeImage, fadeColor);
         tempImage.color = fadeColor;
-        
+
         Color startcolor = tempImage.color;
         Color endcolor = new Color(tempImage.color.r, tempImage.color.g, tempImage.color.b, 0);
-        
+
         float t = 0.0f;
-        
+
         while (tempImage.color.a > 0)
         {
             tempImage.color = Color.Lerp(startcolor, endcolor, t);
@@ -33,45 +35,48 @@ public class UIFadeEffects : MonoBehaviour
             {
                 t += Time.deltaTime / flashDuration;
             }
+
             yield return null;
         }
-        
+
         Destroy(tempImage);
     }
-    
+
     public IEnumerator ImageFlashEvent(Image fadeImage, Color fadeColor, GameObject flashObject)
     {
         var tempImage = TemporaryVariableMaker(fadeImage, fadeColor);
         flashObject.SetActive(true);
         tempImage.color = fadeColor;
-        
+
         Color startcolor = tempImage.color;
         Color endcolor = new Color(tempImage.color.r, tempImage.color.g, tempImage.color.b, 0);
-        
+
         float t = 0.0f;
-        
+
         while (tempImage.color.a > 0)
         {
             tempImage.color = Color.Lerp(startcolor, endcolor, t);
             if (t < 1)
             {
-                t += Time.deltaTime / (flashDuration*2);
+                t += Time.deltaTime / (flashDuration * 2);
             }
+
             yield return null;
         }
+
         flashObject.SetActive(false);
         Destroy(tempImage);
     }
-    
+
     public IEnumerator DoFadeOut(Image fadeImage, Color fadeColor)
     {
         var tempImage = TemporaryVariableMaker(fadeImage, fadeColor);
-        
+
         Color startcolor = tempImage.color;
         Color endcolor = new Color(tempImage.color.r, tempImage.color.g, tempImage.color.b, 1);
-        
+
         float t = 0.0f;
-        
+
         while (tempImage.color.a > 0)
         {
             tempImage.color = Color.Lerp(startcolor, endcolor, t);
@@ -79,22 +84,23 @@ public class UIFadeEffects : MonoBehaviour
             {
                 t += Time.deltaTime / fadeOutDuration;
             }
+
             yield return null;
         }
-        
+
         Destroy(tempImage);
         yield return null;
     }
-    
+
     public IEnumerator DoFadeIn(Image fadeImage, Color fadeColor)
     {
         var tempImage = TemporaryVariableMaker(fadeImage, fadeColor);
-        
+
         Color startcolor = tempImage.color;
         Color endcolor = new Color(tempImage.color.r, tempImage.color.g, tempImage.color.b, 0);
-        
+
         float t = 0.0f;
-        
+
         while (tempImage.color.a > 0)
         {
             tempImage.color = Color.Lerp(startcolor, endcolor, t);
@@ -102,9 +108,10 @@ public class UIFadeEffects : MonoBehaviour
             {
                 t += Time.deltaTime / fadeInDuration;
             }
+
             yield return null;
         }
-        
+
         Destroy(tempImage);
         yield return null;
     }
